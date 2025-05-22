ff21ca08cf4346dcc4959864e873778dff683ed1
Minwoo <cap019@naver.com>
2025-05-22 18:18:00 +0900

    @EnableJpaAuditing 에너테이션 추가

--- BuDongSan/src/main/java/com/fastcampus/BuDongSan/BuDongSanApplication.java
+++ BuDongSan/src/main/java/com/fastcampus/BuDongSan/BuDongSanApplication.java
@@ -2,10 +2,12 @@ package com.fastcampus.BuDongSan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.fastcampus.BuDongSan.repository.jpa") // 👈 JPA는 따로
+@EnableJpaAuditing
 public class BuDongSanApplication {
 
 	public static void main(String[] args) {
