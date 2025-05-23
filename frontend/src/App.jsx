3a7fe0ed59c7d623aa22d09760d688c4dcb5226c
Minwoo <cap019@naver.com>
2025-05-23 18:57:04 +0900

    요소 등록

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,18 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import './css/App.css'
 import Notice from "./component/Notice.jsx";
 import NoticeCreate from "./component/NoticeCreate.jsx";
+import Header from "./component/Header.jsx";
+import Redevelopment from "./component/Redevelopment.jsx";
 
 function App() {
   return (
       <BrowserRouter>
+          <Header />
           <Routes>
-              <Route path="notice" element={<Notice />} />
+              <Route path="/" element={<div>홈페이지입니다</div>} />
+              <Route path="/notice" element={<Notice />} />
               <Route path="/notice/create" element={<NoticeCreate />} />
+              <Route path="/Redevelopment" element={<Redevelopment />} />
           </Routes>
       </BrowserRouter>
   )
