839a7884bcdb0d7c20379848c4b37e560265ed27
Minwoo <cap019@naver.com>
2025-05-26 19:33:04 +0900

    fix: 라우터 컨텍스트 사용중 타입에러가 생기는 부분 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './css/App.css'
-import Notice from "./component/Notice.jsx";
 import NoticeCreate from "./component/NoticeCreate.jsx";
 import Header from "./component/Header.jsx";
 import Redevelopment from "./component/Redevelopment.jsx";
+import NoticePage from "./Notice/NoticePage.jsx";
 
 function App() {
   return (
       <BrowserRouter>
           <Header />
           <Routes>
-              <Route path="/" element={<div>홈페이지입니다</div>} />
-              <Route path="/notice" element={<Notice />} />
+              <Route path="/" element={<h1>메인 화면 준비중입니다.</h1>} />
+              <Route path="/notice" element={<NoticePage />} />
               <Route path="/notice/create" element={<NoticeCreate />} />
               <Route path="/Redevelopment" element={<Redevelopment />} />
           </Routes>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
 import App from './App.jsx'
-import Header from "./component/Header.jsx";
 
-createRoot(document.getElementById('header-root')).render(
-    <Header />
-);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
