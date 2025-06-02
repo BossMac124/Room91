a8bb42f51905b40d9cfb9b81211e42f7f9f9d7fc
chaeyoung <tjcodud6525@naver.com>
2025-06-02 15:23:07 +0900

    fix: 메인페이지 allowedOrigin 주석처리

--- BuDongSan/src/main/java/com/fastcampus/BuDongSan/config/WebConfig.java
+++ BuDongSan/src/main/java/com/fastcampus/BuDongSan/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOriginPatterns("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
