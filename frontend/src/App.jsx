1a831b9cf99ff83148012360e3940f45d2834a21
chaeyoung <tjcodud6525@naver.com>
2025-06-04 11:42:36 +0900

    feat: 공지/FAQ 드롭다운

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./component/Header.jsx";
 import Redevelopment from "./component/Redevelopment/Redevelopment.jsx";
 import NoticePage from "./component/Notice/NoticePage.jsx";
 import FaqCreate from "./component/Faq/FaqCreate";
+import FaqPage from "./component/Faq/FaqPage.jsx";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
               }}>메인 화면 준비중입니다.</h1>} />
               <Route path="/notice" element={<NoticePage />} />
               <Route path="/notice/create" element={<NoticeCreate />} />
+              <Route path="/faq" element={<FaqPage />} />
               <Route path="/faq/create" element={<FaqCreate />} />
               <Route path="/Redevelopment" element={<Redevelopment />} />
           </Routes>
