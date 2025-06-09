ff38193daf901f950020cd30b981f0625d50e52e
chaeyoung <tjcodud6525@naver.com>
2025-06-09 14:21:17 +0900

    feat: Faq에 CKEditor적용

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
+import "./css/ckeditor.css"; // CKEditor 전역 오버라이드
 import App from './App.jsx';
 
 ReactDOM.render(
