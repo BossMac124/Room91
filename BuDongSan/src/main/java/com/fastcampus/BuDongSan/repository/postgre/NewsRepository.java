33807d9d0fc248091a9d2d5963038e673e664696
Minwoo <cap019@naver.com>
2025-07-24 13:21:18 +0900

    fix: @Repository 추가

--- BuDongSan/src/main/java/com/fastcampus/BuDongSan/repository/postgre/NewsRepository.java
+++ BuDongSan/src/main/java/com/fastcampus/BuDongSan/repository/postgre/NewsRepository.java
@@ -2,7 +2,8 @@ package com.fastcampus.BuDongSan.repository.postgre;
 
 import com.fastcampus.BuDongSan.Entity.News;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface NewsRepository extends JpaRepository<News, Long> {
 }
