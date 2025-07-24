ad7fc83b106fb71e8975602ad9dfcc56983f130d
Minwoo <cap019@naver.com>
2025-07-24 11:12:59 +0900

    chore: @EnableJpaRepositories 삭제

--- BuDongSan/src/main/java/com/fastcampus/BuDongSan/BuDongSanApplication.java
+++ BuDongSan/src/main/java/com/fastcampus/BuDongSan/BuDongSanApplication.java
@@ -3,13 +3,8 @@ package com.fastcampus.BuDongSan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = {
-		"com.fastcampus.BuDongSan.repository.mongo",
-		"com.fastcampus.BuDongSan.repository.postgre" // 👈 이거 추가만 하면 해결!
-})
 @EnableJpaAuditing
 public class BuDongSanApplication {
 
